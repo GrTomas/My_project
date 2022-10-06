14f63896a4d27c1b79739a74278dd388e3332150
GrTomas <113602534+GrTomas@users.noreply.github.com>
2022-10-06 20:31:38 +0300

    Create my_func.h

---
+++ my_func.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+#include <numeric>
+#include <fstream>
+#include <sstream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+using std::ifstream;
